a37dc499f8c681bc4555920ce46ba7cb236c32c7
Omeir Mujtaba <omeirr13@gmail.com>
2024-12-06 11:17:43 +0500

    type annotations

---
+++ README.md
@@ -0,0 +1,6 @@
+- Challenge
+- Create a variable of type string and try to invoke a string method on it.
+- Create a variable of type number and try to perform a mathematical operation on it.
+- Create a variable of type boolean and try to perform a logical operation on it.
+- Try to assign a value of a different type to each of these variables and observe the TypeScript compiler's response.
+- You can use type annotation or inference
---
+++ src/tutorial.ts
@@ -0,0 +1,25 @@
+// let name: string = "omeir";
+// name = "hello";
+// //name = 20; will give error
+
+
+// let amount:number = 20;
+// // amount = 'hello'; error
+
+// let flag: boolean = true;
+// flag = Boolean(1);
+// flag = Boolean("hello");
+
+// console.log(name, amount, flag)
+// ;
+
+
+let str: string = "omeir";
+console.log(str.toUpperCase());
+console.log(str.codePointAt(1));
+
+let num: number = 1;
+console.log(num * 10);
+
+let flag: boolean = true;
+console.log(true == flag);
