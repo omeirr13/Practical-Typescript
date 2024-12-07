8f8f6a090401529719e32fe48d9e779002d539ee
Omeir Mujtaba <omeirr13@gmail.com>
2024-12-07 14:35:13 +0500

    intersection

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,28 +1,13 @@
-type Employee = { id: number, name: string, department: string };
-type Manager = { id: number, name: string, employees: Employee[] };
+//intersections are a way to combine multiple types into one.
 
-type Staff = Employee | Manager;
+type Person = { name: string; age: number };
+type Address = { street: string; city: string };
 
+type PersonWithAddress = Person & Address;
 
-const printStaffDetails = (staff: Staff) => {
-    if('employees' in staff){
-        console.log(`You are a manger, you manage ${staff.employees.length} employees.`);
-    }
-    else{
-        console.log(`You are an employee, your name is ${staff.name} you work in department ${staff.department}`);
-    }
-}
-
-let omeir : Employee = {id: 1, name: "omeir", department: "IT"};
-let omar : Employee = {id: 2, name: "omar", department: "IT"};
-
-let belawal : Manager = {id: 3, name: "belawal", employees: [omeir, omar]};
-
-
-const employees : Staff[] = [omeir, omar, belawal];
-
-employees.forEach((employee)=>{
-    printStaffDetails(employee);
-})
-
-
+const personWithAddress: PersonWithAddress = {
+  name: "John",
+  age: 30,
+  street: "123 Main St",
+  city: "New York"
+};
