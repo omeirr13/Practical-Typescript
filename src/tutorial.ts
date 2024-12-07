ff3024990b0e6720f42b2144f67affe858bd57d5
Omeir Mujtaba <omeirr13@gmail.com>
2024-12-07 08:12:06 +0500

    optional default parameters

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,5 +1,34 @@
-const names: string[] = ["omeir", "omar", "ali", "abdullah", "ahmed"];
+const calculatePrice = (price: number, discount?: number): number => {
+    return price - (discount || 0);
+}
+//function with optional parameter must work when they are not supplied.
 
-const exists = (key: string): boolean => names.includes(key);
+let priceAfterDiscount: number = calculatePrice(100, 20);
+//what if sometimes no discount..optional parameter
 
-console.log(exists("omaa"));
+const calculateScore = (initialScore: number, penaltyPoint: number = 0): number => {
+    return initialScore - penaltyPoint;
+}
+
+let scoreWithPenalty: number = calculateScore(100, 20);
+let scoreWithoutPenalty: number = calculateScore(100);
+
+
+function sum(message: string, ...numbers: number[]): string {
+    const doubled: number[] = numbers.map((num) => num * 2);
+
+    let total = numbers.reduce((previous, current) => {
+        return previous + current
+    }, 0);
+
+    return `${message}${total}`;
+}
+
+let result: string = sum('The total is: ', 1, 2, 3, 4, 5);
+console.log(result);
+
+
+function logMessage(message: string): void{
+    console.log(message);
+}
+logMessage("hello");
