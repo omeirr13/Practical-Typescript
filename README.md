65918738a3d3eb4b8d2ca1a841fc9e0625b460a3
Omeir Mujtaba <omeirr13@gmail.com>
2024-12-07 12:36:23 +0500

    object as parameters

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-- Create a variable orderStatus of type 'processing' | 'shipped' | 'delivered' and assign it the value 'processing'. Then, try to assign it the values 'shipped' and 'delivered'.
-- Create a variable discount of type number | string and assign it the value 20. Then, try to assign it the value '20%'.
+Your task is to create a function named processData that accepts two parameters:
+
+- The first parameter, input, should be a union type that can be either a string or a number.
+- The second parameter, config, should be an object with a reverse property of type boolean, by default it "reverse" should be false
+- The function should behave as follows:
+
+- If input is of type number, the function should return the square of the number.
+- If input is of type string, the function should return the string in uppercase.
+- If the reverse property on the config object is true, and input is a string, the function should return the reversed string in uppercase.
--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,17 +1,14 @@
-function createEmployee({ id }: { id: number }): { id: number; isActive: boolean } {
-    return { id: 1, isActive: id % 2 == 0 };
-};
-
-const obj: { id: number, isActive: boolean } = createEmployee({ id: 1 });
-
-
-function createStudent(student: {id: number; name: string}): void {
-    console.log(`Welcome to the course ${student.name.toUpperCase()}!!!`);
+const processData = (input: string | number, config: {reverse: boolean} = {reverse:false}): string | number | void => {
+    if(typeof(input) === "number"){
+        return input**2;
+    }
+    else if(typeof(input) === "string"){
+        let upper = input.toUpperCase();
+        if(config.reverse){
+            upper =  upper.split('').reverse().join('');
+        }
+        return upper;
+    }
 }
 
-const student = {
-    id: 1,
-    name: "omeir"
-}
-createStudent({id: 1, name: "omeir"});//inline matches exactly
-createStudent(student);//this will go ..it just wants the ones to match, extra can come..
+console.log(processData(9));
